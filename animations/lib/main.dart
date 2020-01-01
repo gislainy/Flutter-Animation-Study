1468b58f06894bea6c32beae706f4319660c75e3
Gislainy Crisostomo Velasco <gislainycrisostomo@gmail.com>
2020-01-01 16:56:54 -0300

    Delayed animation

--- animations/lib/main.dart
+++ animations/lib/main.dart
@@ -29,7 +29,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
-  Animation animation;
+  Animation animation, delayedAnimation, muchDelayedAnimation;
   AnimationController animationController;
 
   @override
@@ -40,6 +40,14 @@ class _MyHomePageState extends State<MyHomePage>
 
     animation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
         parent: animationController, curve: Curves.fastOutSlowIn));
+
+    delayedAnimation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+        parent: animationController,
+        curve: Interval(0.5, 1.0, curve: Curves.fastOutSlowIn)));
+
+    muchDelayedAnimation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+        parent: animationController,
+        curve: Interval(0.8, 1.0, curve: Curves.fastOutSlowIn)));
 
     animationController.forward();
   }
@@ -52,15 +60,70 @@ class _MyHomePageState extends State<MyHomePage>
       animation: animationController,
       builder: (context, child) {
         return Scaffold(
-          body: Transform(
-            transform:
-                Matrix4.translationValues(animation.value * width, 0.0, 0.0),
-            child: Center(
-              child: Container(
-                child: Text(
-                  'Login',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
+          body: Center(
+            child: Container(
+              padding: EdgeInsets.all(25),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Transform(
+                    transform: Matrix4.translationValues(
+                        animation.value * width, 0.0, 0.0),
+                    child: Text(
+                      'Login',
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Transform(
+                      transform: Matrix4.translationValues(
+                          delayedAnimation.value * width, 0.0, 0.0),
+                      child: Column(children: <Widget>[
+                        TextField(
+                          decoration: InputDecoration(hintText: 'Username'),
+                        ),
+                        TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            hintText: 'Password',
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                      ])),
+                  Transform(
+                      transform: Matrix4.translationValues(
+                          muchDelayedAnimation.value * width, 0.0, 0.0),
+                      child: Column(children: <Widget>[
+                        RaisedButton(
+                          child: Text('Login'),
+                          color: Colors.blue,
+                          textColor: Colors.white,
+                          onPressed: () {},
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          'Don\'t have on account?',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        OutlineButton(
+                          child: Text('Singup'),
+                          color: Colors.green,
+                          textColor: Colors.green,
+                          borderSide: BorderSide(color: Colors.green),
+                          onPressed: () {},
+                        )
+                      ])),
+                ],
               ),
             ),
           ),
